37f53b04539ac2fa90179fd1a30df36d21cbeb4b
wangyangdong <wangyangdong@unionbigdata.com>
2018-12-26 18:27:02 +0800

    update

--- src/new_test.py
+++ src/new_test.py
@@ -4,3 +4,12 @@ biases = [np.random.randn(y, 1) for y in sizes[1:]]
 print(sizes[:-1])
 weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
 print(weights)
+
+x = 1
+y = 1
+print(int(x==y))
+
+test_data = [[[1,2,3],[3]],[[1,2,3],[4]]]
+for (x, y) in test_data:
+    print(x)
+    print(y)
